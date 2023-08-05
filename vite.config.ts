e7fe6f06ad2e16eaf5ef69eb2b3b9de12990fc6b
David Wong <david@wo.ng>
2023-08-05 21:27:16 +0100

    add images

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,48 @@ import react from '@vitejs/plugin-react-swc'
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
-	registerType: "prompt",
-	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-	manifest: {
-		name: "Travel App",
-		short_name: "TravelApp",
-		description: "An app to test PWAs", 
-		icons: [
-			{
-				src: "/maskable_icon.png",
-				sizes: "225x225",
-				type: "image/png",
-				purpose: "any maskable",
-			},
-		],
-		theme_color: "#171717",
-		background_color: "#e8ebf2",
-		display: "standalone",
-		scope: "/",
-		start_url: "/",
-		orientation: "portrait",
-	},
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Travel App",
+    short_name: "TravelApp",
+    description: "An app to test PWAs",
+    "icons": [
+      {
+        "src": "manifest-icon-192.maskable.png",
+        "sizes": "192x192",
+        "type": "image/png",
+        "purpose": "any"
+      },
+      {
+        "src": "manifest-icon-192.maskable.png",
+        "sizes": "192x192",
+        "type": "image/png",
+        "purpose": "maskable"
+      },
+      {
+        "src": "manifest-icon-512.maskable.png",
+        "sizes": "512x512",
+        "type": "image/png",
+        "purpose": "any"
+      },
+      {
+        "src": "manifest-icon-512.maskable.png",
+        "sizes": "512x512",
+        "type": "image/png",
+        "purpose": "maskable"
+      }
+    ],
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
 };
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./",
-	plugins: [react(), VitePWA(manifestForPlugin)],
+  base: "./",
+  plugins: [react(), VitePWA(manifestForPlugin)],
 });
